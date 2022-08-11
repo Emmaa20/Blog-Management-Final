9f305569bda3b3b85248441716faf85fa020d956
Emmaa20 <elmirafk@code.edu.az>
2022-08-11 16:51:13 +0400

    Enums BlogStatus Class created

---
+++ Blog-Managementt/Blog-Managementt/Database/Enums/BlogStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_Managementt.Database.Enums
+{
+    public enum BlogStatus
+    {
+        Sended = 1,
+        Approved = 2,
+        Rejected = 3
+
+    }
+}
