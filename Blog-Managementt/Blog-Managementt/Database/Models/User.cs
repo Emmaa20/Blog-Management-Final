ddc1ae8672be2b1bc527f6400d62274f938fdf69
Emmaa20 <elmirafk@code.edu.az>
2022-08-11 16:34:15 +0400

    User and User Repository added

--- Blog-Managementt/Blog-Managementt/Database/Models/User.cs
+++ Blog-Managementt/Blog-Managementt/Database/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog_Managementt.Database.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,41 @@ namespace Blog_Managementt.Database.Models
 {
     internal class User
     {
-       
+        public int ID { get; private set; }
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        protected DateTime Time { get; set; } = DateTime.Now;
+
+
+        public User(string name, string surname, string email, string password, int id)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Password = password;
+            ID = id;
+        }
+
+        public User(string name, string surname, string email, string password)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Password = password;
+            ID = UserRepository.IDCounter;
+        }
+
+        public virtual string GetInfo()
+        {
+            return Name + " " + Surname;
+        }
+
+        public virtual string GetFullInfo()
+        {
+            return ID + "" + Name + "" + Surname + "" + Email;
+        }
     }
 }
--- Blog-Managementt/Blog-Managementt/Database/Repository/UserRepository.cs
+++ Blog-Managementt/Blog-Managementt/Database/Repository/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog_Managementt.Database.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,36 @@ namespace Blog_Managementt.Database.Repository
 {
     internal class UserRepository
     {
+        private static int _idcounter;
+
+        public static int IDCounter
+        {
+            get
+            {
+                _idcounter++;
+                return _idcounter;
+            }
+
+        }
+
+        public static List<User> Users { get; set; } = new List<User>()
+        {
+            new User ("Elmira","Kerimova","emma@gmail.com","123"),
+            new User("Nigar","Kerimova","nigga@gmail.com","1234"),
+            new User("Ibrahim","Kerimov","ibo@gmail.com","12345"),
+
+        };
+        public static User Add(string name, string surname, string email, string password)
+        {
+            User user = new User(name, surname, email, password);
+            Users.Add(user);
+            return user;
+        }
+        public static User Add(string name, string surname, string email, string password, int id)
+        {
+            User user = new User(name, surname, email, password, id);
+            Users.Add(user);
+            return user;
+        }
     }
 }
