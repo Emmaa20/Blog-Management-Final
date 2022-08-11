b9c34bf51e452f5ba38efb6857d3b41793684f2f
Emmaa20 <elmirafk@code.edu.az>
2022-08-12 02:12:51 +0400

    added required methods

--- Blog-Managementt/Blog-Managementt/Database/Repository/Common/Repository.cs
+++ Blog-Managementt/Blog-Managementt/Database/Repository/Common/Repository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blog_Managementt.Database.Models.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,45 @@ using System.Threading.Tasks;
 
 namespace Blog_Managementt.Database.Repository.Common
 {
-    internal class Repository
+    public class Repository<TEntity, Tid>
+        where TEntity : Entity<Tid>
     {
+        protected static List<TEntity> DBcontect { get; set; } = new List<TEntity>();
+        public TEntity Add(TEntity entry)
+        {
+            DBcontect.Add(entry);
+            return entry;
+        }
+        public List<TEntity> GetAll()
+        {
+            return DBcontect;
+        }
+        public List<TEntity> GetAll(Predicate<TEntity> predicate)
+        {
+            List<TEntity> entities = new List<TEntity>();
+            foreach (TEntity entity in DBcontect)
+            {
+                if (predicate(entity))
+                {
+                    entities.Add(entity);
+                }
+            }
+            return entities;
+        }
+        public TEntity GetById(Tid id)
+        {
+            foreach (TEntity entry in DBcontect)
+            {
+                if (Equals(entry.Id, id))
+                {
+                    return entry;
+                }
+            }
+            return default(TEntity);
+        }
+        public void Delete(TEntity entry)
+        {
+            DBcontect.Remove(entry);
+        }
     }
 }
