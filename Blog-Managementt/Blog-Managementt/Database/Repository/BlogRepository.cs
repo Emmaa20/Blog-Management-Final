9def2ecf173999cd10298bb278c188fb06919a02
Emmaa20 <elmirafk@code.edu.az>
2022-08-11 17:09:57 +0400

    BlogRepository RandomID added to Blog

--- Blog-Managementt/Blog-Managementt/Database/Repository/BlogRepository.cs
+++ Blog-Managementt/Blog-Managementt/Database/Repository/BlogRepository.cs
@@ -8,5 +8,15 @@ namespace Blog_Managementt.Database.Repository
 {
     internal class BlogRepository
     {
+        static Random randomID = new Random();
+        private static string _id;
+        public static string RandomID
+        {
+            get
+            {
+                _id = "BL" + randomID.Next(1, 6);
+                return _id;
+            }
+        }
     }
 }
