2d08f8a53c36649b2c74ec5e27f676d74968e900
Emmaa20 <elmirafk@code.edu.az>
2022-08-11 20:34:02 +0400

    Inbox model added

--- Blog-Managementt/Blog-Managementt/Database/Models/Inbox.cs
+++ Blog-Managementt/Blog-Managementt/Database/Models/Inbox.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blog_Managementt.Database.Models.Common;
+using Blog_Managementt.Database.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,24 @@ using System.Threading.Tasks;
 
 namespace Blog_Managementt.Database.Models
 {
-    internal class Inbox
-    {
+   
+        public class Inbox : Entity<int>
+        {
+            public string Notfication { get; set; }
+            public User User { get; set; }
+            public Inbox(string notification, User user, int? id = null)
+            {
+                Notfication = notification;
+                User = user;
+                if (id != null)
+                {
+                    Id = id.Value;
+                }
+                else
+                {
+                    Id = UserRepository.IDCounter;
+                }
+            }
+        }
     }
-}
+
