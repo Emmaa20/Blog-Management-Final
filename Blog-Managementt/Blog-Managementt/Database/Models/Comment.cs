141d7eb02320cd9f485ccb1c13ba66e758eb86b6
Emmaa20 <elmirafk@code.edu.az>
2022-08-12 01:57:51 +0400

    Comment model's properties written

--- Blog-Managementt/Blog-Managementt/Database/Models/Comment.cs
+++ Blog-Managementt/Blog-Managementt/Database/Models/Comment.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Blog_Managementt.Database.Models.Common;
+using Blog_Managementt.Database.Repository;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,34 @@ using System.Threading.Tasks;
 
 namespace Blog_Managementt.Database.Models
 {
-    internal class Comment
+
+    public class Comment : Entity<int>
     {
+        public User FromUser { get; set; }
+        public Blog Blog { get; set; }
+        public string Content { get; set; }
+
+        public Comment(User fromUser,Blog blog ,string content, int? id = null)
+
+        {
+            FromUser = fromUser;
+            Content = content;
+            Blog = blog;
+            if (id != null)
+            {
+                Id = id.Value;
+
+            }
+            else
+            {
+                Id = UserRepository.IDCounter;
+            }
+            
+        }
+        public override string ToString()
+        {
+            return $"{FromUser.Name} {FromUser.Surname} {Content}";
+        }
     }
 }
+
