ed11ad3292b57f6bbf89d8feaabcd5822edcab6b
Emmaa20 <elmirafk@code.edu.az>
2022-08-12 02:10:46 +0400

    added required methods

--- Blog-Managementt/Blog-Managementt/Database/Repository/CommentRepository.cs
+++ Blog-Managementt/Blog-Managementt/Database/Repository/CommentRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blog_Managementt.Database.Models;
+using Blog_Managementt.Database.Repository.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Blog_Managementt.Database.Repository
 {
-    internal class CommentRepository
+    public class CommentRepository : Repository<Comment, int>
     {
     }
 }
